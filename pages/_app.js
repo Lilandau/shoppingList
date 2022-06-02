38658e4bcad47ec7cf282b48511d912c908404be
ninapoetzsch <extern.nina.poetzsch@volkswagen.de>
2022-06-02 08:58:56 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Head from "next/head";
+import {useEffect} from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+
+    useEffect(() => {
+        if (typeof document !== undefined) {
+            require("bootstrap/dist/js/bootstrap");
+        }
+    }, []);
+    
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                
+            </Head>
+            
+            <Component {...pageProps} />
+        </>)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import ShoppingElements from "./shoppingElements/shoppingElements";
-
 import styles from './index.module.css';
 
 export default function Home() {
-    return (
+    return (<>
+
+            <div className="shadow p-3 mb-5 bg-white rounded">Regular shadow</div>
         <div className={styles.card}>
             <span className={styles.span}>
                 <h1>Shopping List</h1>
@@ -11,9 +11,8 @@ export default function Home() {
             <span>
                 <img className={styles.img} src={'./icons/shopping-cart.png'} alt={'shoppingCard'}/>
             </span>
-            <div className={styles.shoppingElements}>
-                <ShoppingElements/>
-            </div>
+            
         </div>
+        </>
     )
 }
