697e24948df106be0d2c5629004404193ab8be88
ninapoetzsch <extern.nina.poetzsch@volkswagen.de>
2022-06-03 09:03:08 +0200

    refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,8 @@
-import styles from './index.module.css';
+import ShoppingList from "./shoppingList/shoppingList";
 
 export default function Home() {
     return (<>
-        <div id='shoppingList' className={styles.card}>
-            <span className={styles.span}>
-                <h1>Shopping List</h1>
-            </span>
-            <span>
-                <img className={styles.img} src={'./icons/shopping-cart.png'} alt={'shoppingCard'}/>
-            </span>
-        
-        </div>
+       <ShoppingList/>
         </>
     )
 }
