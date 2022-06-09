799d9281b626175cdee54bbb64e1785c15424940
ninapoetzsch <extern.nina.poetzsch@volkswagen.de>
2022-06-09 10:13:30 +0200

    trashbin is now properly sized

--- pages/shoppingList/shoppingElement.js
+++ pages/shoppingList/shoppingElement.js
@@ -1,11 +1,18 @@
 import styles from './shoppingelement.module.css'
 
-export default function ShoppingElement(){
-    return(
-      <>
-      <div className={styles.shoppingElement}>
-          <p>hallo</p>
-      </div>
-      </>  
-    );
+export default function ShoppingElement() {
+    return (
+        <>
+            <div className={styles.shoppingElement}>
+                <div className="input-group mb-3">
+                    <button className={"btn btn-outline-secondary"} type="button" id="button-addon1">
+                        <img  className={styles.btnImg} src={'./icons/trash.png'} alt={'trashbin'}></img>
+                    </button>
+                    <input type="text" className="form-control" placeholder=""
+                           aria-label="Example text with button addon"
+                           aria-describedby="button-addon1"/>
+                </div>
+            </div>
+        </>
+);
 }
