036712105b6b776d9488a10348744570adc1f560
ninapoetzsch <extern.nina.poetzsch@volkswagen.de>
2022-07-13 13:17:09 +0200

    changed type of ShoppingElement from input to div

--- pages/shoppingList/shoppingElement.js
+++ pages/shoppingList/shoppingElement.js
@@ -2,22 +2,23 @@ import styles from './shoppingelement.module.css'
 import {useState} from "react";
 
 export default function ShoppingElement(props) {
-    const [element= {name, amount, id}, change] = useState(props);
+    const [element = {name, amount, id}, change] = useState(props);
 
 
-    
     return (
         <>
             <li className={styles.shoppingElement}>
-                <div >
+                <div>
                     <div className="input-group mb-3">
                         <button className={"btn btn-outline-secondary"} type="button" id="button-addon1"
-                                onClick={()=>props.handleDelete(element.id)}>
+                                onClick={() => props.handleDelete(element.id)}>
                             <img className={styles.btnImg} src={'./icons/trash.png'} alt={'trashbin'}></img>
                         </button>
-                        <input type="text" className="form-control" value={props.name}
-                               aria-label="Example text with button addon"
-                               aria-describedby="button-addon1"/>
+                        <div type="text" className={` ${styles.elementFieldDiv} form-control`}
+                             aria-label="Example text with button addon"
+                             aria-describedby="button-addon1">
+                            <p className={styles.elementField}>{element.name}</p>
+                        </div>
                     </div>
                 </div>
             </li>
